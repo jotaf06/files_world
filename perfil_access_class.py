2f92c0b83fd702bc6d645f43987ae97155efa535
José Félix <115491600+jotaf06@users.noreply.github.com>
2023-08-23 11:58:53 -0300

    Update perfil_access_class.py
    Utilização do Try e Except para verificação de erros

--- perfil_access_class.py
+++ perfil_access_class.py
@@ -2,23 +2,24 @@ from find_user_bynick_class import FindUser
 
 class PerfilAccess():
     def access(self, users, user):
-        nickname = input("\nForneça o nickname do perfil: ")
-        a_user = FindUser().find(users, nickname)
+        try:
+            nickname = input("\nForneça o nickname do perfil: ")
+            a_user = FindUser().find(users, nickname)
 
-        if a_user == None:
-            print("\nNão há usuário com esse perfil.")
-        
-        elif a_user.privacity == 1:
-            print("\nEsse perfil é privado.")
-            if user in a_user.friends:
-                return a_user
-            else:
-                print("Você não tem acesso a esse perfil.")
-                return None
-    
-        elif a_user.privacity == 0:
-            print("\nEsse perfil é aberto.")
-            return a_user
-
-
+            if a_user == None:
+                raise Exception("Não há usuário com esse perfil.")
             
+            elif a_user.privacity == 1:
+                print("Esse perfil é privado.")
+                if user in a_user.friends:
+                    return a_user
+                else:
+                    raise Exception("Você não tem acesso a esse perfil.")
+        
+            elif a_user.privacity == 0:
+                print("\nEsse perfil é aberto.")
+                return a_user
+                
+        except Exception as erro:
+            print("Ocorreu um erro na operação de acesso: ", erro)
+
